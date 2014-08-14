f477a9c4d1747c204fc965f5d7f096f48a307675
Joshua Howland <development@jkhowland.com>
2014-08-14 12:20:44 -0600

    Adds initial Swift Project

---
+++ AGoodStart-Swift/AGoodStart-Swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AGoodStart-Swift
+//
+//  Created by Joshua Howland on 8/14/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+
+        // Insert code here...
+        
+        
+        
+        return true
+    }
+
+}
+
